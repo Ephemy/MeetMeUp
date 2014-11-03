2f74ee45d4f6e79ca0bee3820d90f4dc26520299
Jonathan Chou <jonathanchou@Jonathans-MacBook-Pro-2.local>
2014-11-03 13:19:52 -0800

    Initial Commit

---
+++ MeetMeUp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetMeUp
+//
+//  Created by Jonathan Chou on 11/3/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeetMeUp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetMeUp
+//
+//  Created by Jonathan Chou on 11/3/14.
+//  Copyright (c) 2014 Jonathan Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
